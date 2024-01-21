f9bbfae9f39f1ae83625448faf5f22791840ab00
Your Name <you@example.com>
2024-01-21 16:21:14 +0700

    Add new project event

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react";
+
 import NewProject from "./component/NewProject.jsx";
+import NoProjectSelected from "./component/NoProjectSelected.jsx";
 import ProjectSidebar from "./component/ProjectSidebar.jsx";
 
+
 function App() {
+  const [projectsState, setprojectsState] = useState(
+    {
+      selectedProjectId: undefined,
+      projects: []
+    }
+  )
+
+
+  function handleStartAddProject() {
+    setprojectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    }
+    )
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   )
 }
