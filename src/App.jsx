beb18c1cf5fa1c25c030aa8e5a376be46fc2d376
Your Name <you@example.com>
2024-01-21 21:14:21 +0700

    Add new project

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,37 @@ function App() {
         ...prevState,
         selectedProjectId: null,
       }
-    }
-    )
+    })
+  }
+
+  function handleAddProject(projectData) {
+    setprojectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      }
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      }
+
+    })
   }
 
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject} />
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
       {content}
     </main>
   )
--- src/component/NewProject.jsx
+++ src/component/NewProject.jsx
@@ -1,16 +1,34 @@
-import Input from "./Input"
+import { useRef } from "react";
+import { Ref } from "react";
+import Input from "./Input.jsx";
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    function handleSave() {
+        const enterTitle = title.current.value;
+        const enterDescription = description.current.value;
+        const enterDueDate = dueDate.current.value;
+
+        onAdd({
+            title: enterTitle,
+            enterDescription: enterDescription,
+            enterDueDate: enterDueDate,
+        })
+    }
+
     return (
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-                <li><button className="px-6 by-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+                <li><button onClick={handleSave} className="px-6 by-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
             </menu>
             <div>
-                <Input label={'Title'} />
-                <Input label={'Description'} textarea />
-                <Input label={'Due Date'} />
+                <Input ref={title} label={'Title'} />
+                <Input ref={description} label={'Description'} textarea />
+                <Input ref={dueDate} label={'Due Date'} />
             </div>
         </div>
     )
